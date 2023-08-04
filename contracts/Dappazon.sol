5d94193bbad1a3213cf681eed263be744d51234b
PoMaMa69 <saswatapatra15@gmail.com>
2023-08-04 22:35:33 +0530

    test setup done

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+    }
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,25 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappazon", () => {
+  let dappazon;
+  let deployer,buyer;
 
+  beforeEach(async()=>{
+    [deployer,buyer] = await ethers.getSigners();
+
+    //Deploy the smart contract
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+  })
+  describe("Deployment", () =>{
+    it("sets an owner", async()=>{
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    })
+  })
+  
 })
